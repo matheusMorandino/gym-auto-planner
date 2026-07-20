ba9ca10fd1d886feb294aa7da7656797cc21e919
matheus <matheus2060@gmail.com>
2026-07-20 20:12:10 -0300

    feat: Creating vector representation for muscle usage in the Exercise object

--- src/consts/mapping.py
+++ src/consts/mapping.py
@@ -8,6 +8,24 @@ from src.models.data_models import Exercise, Muscle, Equipment, MuscleGroup
 ROOT_DIR = os.path.dirname(os.path.dirname(os.getcwd()))
 
 
+def __extract_list_from_str(text: str, sep: str = ',', none_ignore: bool = True) -> List[str]:
+    """
+    Extracts a list of strings from a string, using the given separator.
+    :param text: The input string to be split.
+    :param sep: The separator to use for splitting the string.
+    :param none_ignore: If True, ignores empty strings in the result.
+    :return: A list of strings extracted from the input string.
+    """
+    if not isinstance(text, str):
+        return []
+
+    result = [item.strip() for item in text.split(sep)]
+    if none_ignore:
+        result = [item for item in result if item and item != 'None']
+
+    return result
+
+
 def __build_training_list(df_raw: pd.DataFrame) -> Dict[Tuple[str, str], Exercise]:
     """
     Returns a Dict of Exercise objects
@@ -33,11 +51,74 @@ def __build_training_list(df_raw: pd.DataFrame) -> Dict[Tuple[str, str], Exercis
         lambda x: [
             ' '.join(m.split())
             for m
-            in (x['Secondary Muscles'].split(',') if isinstance(x['Secondary Muscles'], str) else [])
+            in __extract_list_from_str(x['Secondary Muscles'])
             if len(m.strip()) > 0
         ], axis=1)
 
-    df_raw = df_raw[['name', 'equipment', 'primary_muscles', 'secondary_muscles']]
+    # Normalize name of synergistic_muscles
+    df_raw['synergistic_muscles'] = df_raw.apply(
+        lambda x: [
+            ' '.join(m.split())
+            for m
+            in __extract_list_from_str(x['Synergist_Muscles'])
+            if len(m.strip()) > 0
+        ], axis=1)
+
+    # Normalize name of stabilizing_muscles
+    df_raw['stabilizing_muscles'] = df_raw.apply(
+        lambda x: [
+            ' '.join(m.split())
+            for m
+            in __extract_list_from_str(x['Stabilizer_Muscles'])
+            if len(m.strip()) > 0
+        ], axis=1)
+
+    # Normalize name of antagonist_muscles
+    df_raw['antagonist_muscles'] = df_raw.apply(
+        lambda x: [
+            ' '.join(m.split())
+            for m
+            in __extract_list_from_str(x['Antagonist_Muscles'])
+            if len(m.strip()) > 0
+        ], axis=1)
+
+    # Normalize name of dynamic_muscles
+    df_raw['dynamic_muscles'] = df_raw.apply(
+        lambda x: [
+            ' '.join(m.split())
+            for m
+            in __extract_list_from_str(x['Dynamic_Stabilizer_Muscles'])
+            if len(m.strip()) > 0
+        ], axis=1)
+
+    df_raw = df_raw[[
+        'name',
+        'equipment',
+        'primary_muscles',
+        'secondary_muscles',
+        'synergistic_muscles',
+        'stabilizing_muscles',
+        'antagonist_muscles',
+        'dynamic_muscles'
+    ]]
+
+    # Building muscle usage vector
+    df_raw['muscle_vector'] = df_raw.apply(
+        lambda x: {
+            muscle:
+            1
+                if muscle
+                in (x['primary_muscles'] +
+                 x['secondary_muscles'] +
+                 x['synergistic_muscles'] +
+                 x['stabilizing_muscles'] +
+                 x['antagonist_muscles'] +
+                 x['dynamic_muscles'])
+                else 0
+            for muscle
+            in MUSCLES_DICT.keys()},
+        axis=1
+    )
 
     return {(row['name'], row['equipment']): Exercise(**row) for _, row in df_raw.iterrows()}
 
@@ -64,9 +145,6 @@ def __get_all_muscle_groups(mapping_path: str) -> Dict[str, MuscleGroup]:
 # Raw dataframe with exercise data
 RAW_EXERCISE_TABLE = pd.read_csv(os.path.join(ROOT_DIR, 'datasets', 'gym_exercise_dataset.csv'))
 
-# Dict listing all the exercises using the Exercise dataclass
-EXERCISE_DICT = __build_training_list(df_raw=RAW_EXERCISE_TABLE)
-
 # Mappings for muscle and muscle groups in the SVG image
 MUSCLE_GROUPS = __get_all_muscle_groups(mapping_path=os.path.join(ROOT_DIR, 'datasets', 'muscle_mapping.json'))
 
@@ -74,8 +152,8 @@ MUSCLE_GROUPS = __get_all_muscle_groups(mapping_path=os.path.join(ROOT_DIR, 'dat
 MUSCLES_DICT = {
     muscle.name: muscle for muscle in [muscle for group in MUSCLE_GROUPS.values() for muscle in group.components]}
 
+# Dict listing all the exercises using the Exercise dataclass
+EXERCISE_DICT = __build_training_list(df_raw=RAW_EXERCISE_TABLE)
+
 # Dict listing of all equipments available across all exercises
 EQUIPMENT_DICT = __get_all_equipments(df_raw=RAW_EXERCISE_TABLE)
-
-# M constant for Big-M constrain
-M: int = 1e4
--- src/models/data_models.py
+++ src/models/data_models.py
@@ -1,15 +1,14 @@
-from typing import List
+from typing import List, Dict
 from dataclasses import dataclass, field
 
 
 @dataclass
 class Muscle:
     name: str = field(default='')
-    target_strain: int = field(default=0)
 
 
 @dataclass
-class MuscleGroup:
+class MuscleGroup():
     name: str = field(default='')
     components: List[Muscle] = field(default_factory=list)
 
@@ -21,14 +20,20 @@ class Equipment:
 
 @dataclass
 class Exercise:
+    # Exercise attributes
     name: str = field(default='')
     equipment: Equipment = field(default_factory=Equipment)
+
+    # Muscle groupings
     primary_muscles: List[Muscle] = field(default_factory=list)
     secondary_muscles: List[Muscle] = field(default_factory=list)
     synergistic_muscles: List[Muscle] = field(default_factory=list)
     stabilizing_muscles: List[Muscle] = field(default_factory=list)
     antagonist_muscles: List[Muscle] = field(default_factory=list)
     dynamic_muscles: List[Muscle] = field(default_factory=list)
+
+    # Modeling representations
+    muscle_vector: Dict[Muscle, int] = field(default_factory=dict)
 
 
 @dataclass

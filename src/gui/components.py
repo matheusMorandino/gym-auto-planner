c4ae1e60c246b40c26c6c194a03011c8fd6a5eb9
matheus <matheus2060@gmail.com>
2026-08-09 17:40:22 -0300

    feat: New reusable component for multi item selection

--- src/gui/components.py
+++ src/gui/components.py
@@ -1,4 +1,5 @@
-from typing import Callable, Optional
+from gc import enable
+from typing import Callable, Optional, List
 
 import flet as ft
 
@@ -77,8 +78,88 @@ class SliderCard(ft.Card):
             return
 
         self.value_text.value = f"{current_value:g}"
+
         if self.on_value_change:
             self.on_value_change(current_value)
+
         if e.page:
             e.page.update()
 
+
+@ft.control
+class MultiSelectSearchCard(ft.Card):
+    """
+    Builds a reusable multi-select search card with title, description, and callback.
+    """
+    items: List[str] = None
+
+    def __init__(
+        self,
+        title: str,
+        description: str,
+        options: list[str],
+        on_value_change: Optional[Callable[[List[str]], None]] = None,
+        width: int = 360,
+    ):
+        self.on_value_change = on_value_change
+
+        self.dropdown = ft.Dropdown(
+            options=[ft.DropdownOption(option) for option in options],
+            hint_text="Select options",
+            width=width - 32,
+            on_select=self._update_items_list,
+            editable=True,
+            enable_filter=True,
+        )
+
+        self.chips = ft.Row([], wrap=True)
+
+        super().__init__(
+            content=ft.Container(
+                width=width,
+                padding=16,
+                content=ft.Column(
+                    [
+                        ft.Text(title, size=16, weight=ft.FontWeight.BOLD),
+                        ft.Text(description, size=12, color=ft.Colors.GREY_700),
+                        self.dropdown,
+                        self.chips
+                    ],
+                    tight=True,
+                    spacing=10,
+                ),
+            )
+        )
+
+    def _update_items_list(self, e: ft.Dropdown):
+        val = e.control.value
+
+        if (self.items is None) or (val not in self.items):
+            if self.items is None:
+                self.items = [val]
+            elif not val in self.items:
+                self.items.append(val)
+
+            # Update chips
+            self.chips.controls.append(
+                ft.Chip(
+                    label=val,
+                    on_delete=lambda e, value=val: self._remove_item(e, value)
+                )
+            )
+
+        # Update callback reference
+        if self.on_value_change:
+            self.on_value_change(self.items)
+
+    def _remove_item(self, e: ft.Event, value: str):
+        if self.items is not None and value in self.items:
+            self.items.remove(value)
+        self.chips.controls = [chip for chip in self.chips.controls if chip.label != value]
+
+        # Update callback reference
+        if self.on_value_change:
+            self.on_value_change(self.items)
+
+        if e.page:
+            e.page.update()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import flet as ft
 
-from gui.components import SliderCard
+from src.consts.mapping import EQUIPMENT_DICT
+from gui.components import SliderCard, MultiSelectSearchCard
 
 
 scenario_params = {
@@ -18,6 +19,19 @@ scenario_params = {
         "min": 0,
         "max": 5,
     },
+    "similarity_threshold": {
+        "value": 0.4,
+        "title": "Similarity threshold",
+        "description": "Threshold for considering exercises as too similar.",
+        "min": 0,
+        "max": 1,
+    },
+    "valid_equipments": {
+        "value": [],
+        "options": list(EQUIPMENT_DICT.keys()),
+        "title": "Valid equipments",
+        "description": "Choose the equipments you want to include in your workout. If left empty all equipments will be included.",
+    }
 }
 
 
@@ -38,6 +52,28 @@ def build_param_slider(key: str) -> ft.Card:
     )
 
 
+def build_equipment_card():
+    return MultiSelectSearchCard(
+        title=scenario_params["valid_equipments"]["title"],
+        description=scenario_params["valid_equipments"]["description"],
+        options=scenario_params["valid_equipments"]["options"],
+        on_value_change=lambda value: scenario_params["valid_equipments"].update({"value": value}),
+    )
+
+
+def build_print_button():
+    """
+    Creates button that prints all value present in scenario_parameters when pressed. This is for testing purposes only.
+    :return: flet.Button
+    """
+    def on_click(e):
+        print("Current scenario parameters:")
+        for key, value in scenario_params.items():
+            print(f"{key}: {value['value'] if 'value' in value else value}")
+
+    return ft.ElevatedButton("Print scenario parameters", on_click=on_click)
+
+
 def main(page: ft.Page):
     page.title = "Exercise auto planner"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
@@ -47,10 +83,22 @@ def main(page: ft.Page):
             [
                 build_param_slider("training_target"),
                 build_param_slider("overtraining_delta"),
+                build_param_slider("similarity_threshold"),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
-
+        ft.Row(
+            [
+                build_equipment_card()
+            ],
+            alignment = ft.MainAxisAlignment.CENTER,
+        ),
+        ft.Row(
+            [
+                build_print_button()
+            ],
+            alignment = ft.MainAxisAlignment.CENTER,
+        )
     )
 
 
